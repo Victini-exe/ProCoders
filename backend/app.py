c9a2555391a56194c58b2a4efda04314f316cea6
harini-saravanan-glitch <146167071+harini-saravanan-glitch@users.noreply.github.com>
2025-05-18 10:22:28 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,23 @@
 from flask import Flask
-from application import create_app
+from flask_cors import CORS
+from routes import api_routes
+from flask_jwt_extended import JWTManager
 
-app = create_app()
+app = Flask(__name__)
+CORS(app)
+app.config['JWT_SECRET_KEY'] = 'super-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-if __name__ == "__main__":
-    app.run()
+jwt = JWTManager(app)
+
+from models import db
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+app.register_blueprint(api_routes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
