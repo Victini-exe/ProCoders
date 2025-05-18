af2527ef71ee34a5fc859666a99aefb555a2850d
krshnndu <krishnenducodes@gmail.com>
2025-05-18 13:12:43 +0530

    updated product resource

--- backend/application/resources/product.py
+++ backend/application/resources/product.py
@@ -1,32 +1,37 @@
+from flask import request
 from flask_restful import Resource, reqparse
-from flask_security import auth_required, current_user, roles_required, roles_accepted
+from flask_security import auth_required, current_user
 from ..database import db
 from ..models import Product
 
 class ProductListResource(Resource):
     def __init__(self):
         self.parser = reqparse.RequestParser()
-        self.parser.add_argument('title', type=str, required=True)
-        self.parser.add_argument('description', type=str, required=True)
-        self.parser.add_argument('price', type=float, required=True)
-        self.parser.add_argument('quantity', type=int, required=True)
-        self.parser.add_argument('condition', type=str, required=True)
-        self.parser.add_argument('yome', type=str, required=True)
-        self.parser.add_argument('brand', type=str, required=True)
-        self.parser.add_argument('model', type=str, required=True)
-        self.parser.add_argument('dimensions', type=str, required=True)
-        self.parser.add_argument('weight', type=float, required=True)
-        self.parser.add_argument('material', type=str, required=True)
-        self.parser.add_argument('original_packaging', type=bool, required=True)
-        self.parser.add_argument('manual_included', type=bool, required=True)
-        self.parser.add_argument('working_condition_desc', type=str, required=True)
+        self.parser.add_argument('title', type=str, required=True, help='Title is required')
+        self.parser.add_argument('description', type=str, required=True, help='Description is required')
+        self.parser.add_argument('price', type=float, required=False)
+        self.parser.add_argument('quantity', type=int, required=False)
+        self.parser.add_argument('condition', type=str, required=False)
+        self.parser.add_argument('yome', type=str, required=False)
+        self.parser.add_argument('brand', type=str, required=False)
+        self.parser.add_argument('model', type=str, required=False)
+        self.parser.add_argument('dimensions', type=str, required=False)
+        self.parser.add_argument('weight', type=float, required=False)
+        self.parser.add_argument('material', type=str, required=False)
+        self.parser.add_argument('original_packaging', type=bool, required=False)
+        self.parser.add_argument('manual_included', type=bool, required=False)
+        self.parser.add_argument('working_condition_desc', type=str, required=False)
         self.parser.add_argument('image_url', type=str, required=False)
-        self.parser.add_argument('category_id', type=int, required=True)
+        self.parser.add_argument('category_id', type=int, required=False)
 
     @auth_required()
     def get(self):
-        products = Product.query.all()
-        return [p.__dict__ for p in products], 200
+        category = request.args.get('category_id')
+        query = Product.query
+        if category:
+            query = query.filter_by(category_id=category)
+        products = query.all()
+        return [product.to_dict() for product in products], 200
 
     @auth_required()
     def post(self):
@@ -34,25 +39,25 @@ class ProductListResource(Resource):
         product = Product(
             title=args['title'],
             description=args['description'],
-            price=args['price'],
-            quantity=args['quantity'],
-            condition=args['condition'],
-            yome=args['yome'],
-            brand=args['brand'],
-            model=args['model'],
-            dimensions=args['dimensions'],
-            weight=args['weight'],
-            material=args['material'],
-            original_packaging=args['original_packaging'],
-            manual_included=args['manual_included'],
-            working_condition_desc=args['working_condition_desc'],
+            price=args.get('price'),
+            quantity=args.get('quantity'),
+            condition=args.get('condition'),
+            yome=args.get('yome'),
+            brand=args.get('brand'),
+            model=args.get('model'),
+            dimensions=args.get('dimensions'),
+            weight=args.get('weight'),
+            material=args.get('material'),
+            original_packaging=args.get('original_packaging'),
+            manual_included=args.get('manual_included'),
+            working_condition_desc=args.get('working_condition_desc'),
             image_url=args.get('image_url'),
             seller_id=current_user.id,
-            category_id=args['category_id']
+            category_id=args.get('category_id')
         )
         db.session.add(product)
         db.session.commit()
-        return product.__dict__, 201
+        return product.to_dict(), 201
 
 
 class ProductResource(Resource):
@@ -62,20 +67,21 @@ class ProductResource(Resource):
     @auth_required()
     def get(self, product_id):
         product = Product.query.get_or_404(product_id)
-        return product.__dict__, 200
+        return product.to_dict(), 200
 
     @auth_required()
     def put(self, product_id):
-        args = self.parser.parse_args()
         product = Product.query.get_or_404(product_id)
         if product.seller_id != current_user.id:
             return {'message': 'Unauthorized'}, 403
 
+        args = self.parser.parse_args()
         for key, value in args.items():
-            setattr(product, key, value)
+            if value is not None:
+                setattr(product, key, value)
 
         db.session.commit()
-        return product.__dict__, 200
+        return product.to_dict(), 200
 
     @auth_required()
     def delete(self, product_id):
